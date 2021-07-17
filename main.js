4997885966d08d713a7819f043364e02f232a6b7
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2021-07-17 12:44:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function one(){
+    window.alert("THE STATUE")
+}
+
+function two(){
+    window.alert("THE IDLE WITH NO WORK 😂")
+}
+
+function three(){
+    window.alert("THE VICTORY")
+}
+
+function four(){
+    window.alert("THE YO")
+}
+
+function five(){
+    window.alert("THE GANGSTER")
+}
+
+function six(){
+    window.alert("SRI KRISHNA")
+}
+
+function seven(){
+    window.alert("HALLOWEEN")
+}
